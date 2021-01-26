74b03de1790d659cf14df55c32417774fd5421f5
Serghei Iakovlev <egrep@protonmail.ch>
2021-01-26 22:50:44 +0200

    Enable Python 3.6 support

--- setup.py
+++ setup.py
@@ -173,7 +173,7 @@ if __name__ == '__main__':
         platforms='any',
         include_package_data=True,
         zip_safe=False,
-        python_requires='>=3.7, <4',
+        python_requires='>=3.6, <4',
         install_requires=INSTALL_REQUIRES,
         extras_require=EXTRAS_REQUIRE,
     )
